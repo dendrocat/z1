97ef536bd74b5048d8e4cbc777ece724be74d98b
dendrocat <98703018+dendrocat@users.noreply.github.com>
2023-06-09 22:50:24 +0300

    Update Header.h

--- f/Header.h
+++ f/Header.h
@@ -1,9 +1,9 @@
 #pragma once
 
-int my_strlen(char* s);
+int my_strlen(const char* s);
 
-void my_strcpy(char* dest, char* s);
+void my_strcpy(char* dest, const char* s);
 
-void my_strcat(char* dest, char* s);
+void my_strcat(char* dest, const char* s);
 
-int my_strcmp(char* s1, char* s2);
+int my_strcmp(const char* s1, const char* s2);
